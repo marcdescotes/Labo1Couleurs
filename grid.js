3278baa6a8ef0d67784bc8d0a3426e83c9eae73c
Marc-André Descôteaux <marcdescotes@gmail.com>
2023-06-08 21:10:35 -0400

    commentaires

--- grid.js
+++ grid.js
@@ -20,36 +20,39 @@ function getRandomColor() {
 }
 
 
+
 function updateGridSize() {
-  gridSize = parseInt(gridSizeInput.value);
-  currentSizeText.textContent = `${gridSize}x${gridSize}`;
+  gridSize = parseInt(gridSizeInput.value); // Va chercher la taille de la grille demandée par l'utilisateur et la convertit en nombre entier
+  currentSizeText.textContent = `${gridSize}x${gridSize}`; // Met à jour le texte indiquant la taille actuelle de la grille
 
-  container.innerHTML = '';
+  container.innerHTML = ''; // Vide les couleurs de la grille quand l'utilisateur change de taille
 
-  container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-  container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+  /* https://css-tricks.com/introduction-fr-css-unit/ */
+  container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`; // Définit le nombre de colonnes de la grille en fonction de la taille actuelle
+  container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`; // Définit le nombre de lignes de la grille en fonction de la taille actuelle
 
-  for (let i = 0; i < gridSize * gridSize; i++) {
-    const gridSquare = document.createElement('div');
-    gridSquare.classList.add('gridSquare');
-    container.appendChild(gridSquare);
+  for (let i = 0; i < gridSize * gridSize; i++) { // Boucle pour créer les carrés de la grille en fonction de la taille actuelle
+    const gridSquare = document.createElement('div'); // Crée un nouvel élément div pour représenter un carré de la grille
+    gridSquare.classList.add('gridSquare'); // Ajoute la classe 'gridSquare' à l'élément div
+    container.appendChild(gridSquare); // Ajoute le carré de la grille au conteneur (container)
   }
 }
 
-function handleSquareChange(event) {
-  const square = event.target;
 
-  if (square.classList.contains('gridSquare')) {
-    if (colors.length > 0) {
-      const randomColor = colors[Math.floor(Math.random() * colors.length)];
-      square.style.backgroundColor = randomColor;
+function handleSquareChange(event) {
+  const square = event.target; // Récupère l'élément carré sur lequel l'événement a été déclenché
+
+  if (square.classList.contains('gridSquare')) { // Vérifie si le carré possède la classe 'gridSquare'
+    if (colors.length > 0) { // Vérifie si l'utilisateur a inséré des couleurs dans la palette
+      const randomColor = colors[Math.floor(Math.random() * colors.length)]; // Génère une couleur aléatoire à partir des couleurs sélectionnées dans la palette
+      square.style.backgroundColor = randomColor; // Applique la couleur aléatoire au fond du carré
     } else {
-      square.style.backgroundColor = getRandomColor();
+      square.style.backgroundColor = getRandomColor(); // Si la palette est vide, applique une couleur aléatoire
     }
   }
 }
 
-
+// Fonction permettant à l'utilisateur d'ajouter des couleurs à la palette
 function addColor() {
   const color = colorPicker.value;
   colors.push(color);
